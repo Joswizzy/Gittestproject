c4907f97c58a9c84f327858a115c5caa1ab1e402
Joswizzy <josue.a.koenig@gmail.com>
2023-08-30 13:35:23 -0500

    Added a new Profile domain object
    ,,,

---
+++ src/com/coderscampus/domain/Profile.java
@@ -0,0 +1,28 @@
+package com.coderscampus.domain;
+
+//this is just a tutorial code for learning**
+
+public class Profile {
+	private Integer id;
+	private String name;
+	private String url;
+}
+
+	public Integer getId() {
+		return id;
+	}
+	
+	public Integer setId(int id) {
+		this.id = id;
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String getUrl() {
+		return url;
+	}
+	
+	
+}
